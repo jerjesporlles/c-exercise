61afb955c4222dff775a7635281cd3113b2ad0f9
Jerjes PORLLES <67653943+jerjesporlles@users.noreply.github.com>
2022-08-06 11:08:23 -0500

    Add files via upload

---
+++ calculos.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout <<"1233+1234= "<<1233+1234<<endl;
+    cout<<"1234-111.5= " <<1234-111.5<<endl;
+    cout<<"34*59= "<<34*59<<endl;
+    cout<<"3.4*5.93= "<<3.4*5.93<<endl;
+    cout<<"500/7= "<<500/7<<endl;
+    return 0;
+ 
+}
